0cf6590abc89cf6a7d862490acceb42249f076dc
Master Jumblespeed <sctigercat1@gmail.com>
2016-04-17 02:33:22 -0400

    Adjust trigger word formulation; capitalize parseString except literal url

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-#Copyright © /u/sagiksp 2016, all right reserved.
+#Copyright: Copyright 2016 /u/sagiksp, all right reserved.
 #Seriously, don't be a dick.
 
-import praw, time
+import praw, time, re
 
 #Variables
 
@@ -13,27 +13,26 @@ users = [] #Array of all people that used the bot, and times of use. Format: [[U
 
 yelled = []
 
-Triggers = ['What', 'what', 'WHAT', 'What?', 'what?', 'WHAT?',
-             'Wut', 'wut', 'WUT', 'Wut?', 'wut?', 'WUT?',
-             'Wat', 'wat', 'WAT', 'Wat?', 'wat?', 'WAT?'] #Texts that trigger the bot.
+Triggers = ['what', 'wut', 'wat']
+Triggers += list(word + '?' for word in Triggers) # append ? to the ends of each of the words above
 
 footer = """***
 
 [^^^Beep ^^^boop.](https://np.reddit.com/r/CantHearYouBot/)""" #Text to be in the end of a message
 
 def check_condition(c): #Check the bot condition
-    return (c.body in Triggers) and (not RateLimit(c.author.name)) #Is the comment a trigger, and is the author not rate limited.
+    return (c.body.lower() in Triggers) and (not RateLimit(c.author.name)) #Is the comment a trigger, and is the author not rate limited.
 
 def bot_action(c,r): #Action the bot preforms
     global users
     parent = r.get_info(thing_id=c.parent_id) #get parent comment
+
     if isNotAValidComment(parent): return #If it is a response to a thread, stop.
 
     if parent.author.name == username and parent.body != "In da but": #If parent is bot and comment is not "in da but"
         return
-    
+
     users.append([c.author.name,c.created_utc]) #Add username and time of use to the users list
-    
 
 
     if check_condition(parent): #What What
@@ -42,7 +41,7 @@ def bot_action(c,r): #Action the bot preforms
         except:
             pass
         return
-    
+
     lines = parent.body.split("\n") #Split parent into lines
     total = ""
     for line in lines: #for each line
@@ -52,7 +51,6 @@ def bot_action(c,r): #Action the bot preforms
         print("\n"+total+"\n\n") #Debug
     except:
         return
-    
 
 def findUsersWithName(name): #find all people with username on the users list
     uses = []
@@ -63,7 +61,7 @@ def findUsersWithName(name): #find all people with username on the users list
 
 def getLastTimeOfUse(name): #Get last time a username has used the bot
     return findUsersWithName(name)[-1][1]
-    
+
 def RateLimit(username): #Boolean. if user has used the bot in the last 5 minutes, stop.
     if username == "sagiksp": return False #unlimited testing
     c_time = time.time() #Current time
@@ -72,15 +70,30 @@ def RateLimit(username): #Boolean. if user has used the bot in the last 5 minute
     lastUseTime = getLastTimeOfUse(username) #Latest time of use
     return (c_time - lastUseTime) <= 300 #has the user used the bot in the last 300 seconds (5 minutes)
 
-
 def parseLine(line):
-    #For the gods of python above, please make a switch statement.
-    if line == "": return "\n" 
-    if line[0] ==  "#": return line.upper() + "\n" #If line already bolded, ignore.
-    if line == "***": return "***\n" #If line is *** (Horizontal rule), ignore.
-    if line == "[^^^Beep ^^^boop.](https://np.reddit.com/r/CantHearYouBot/)": return "#[^^^BEEP ^^^BOOP.](https://np.reddit.com/r/CantHearYouBot/)\n" #TODO: Make links work. Currently it capitalizes the link address. A work around for the subreddit link
-    if line == "#[^^^BEEP ^^^BOOP.](https://np.reddit.com/r/CantHearYouBot/)": return line + "\n" #Here too. It's not working. Send help.
-    return "#" + line.upper() + "\n" #For normal lines, bold it and go home.
+    # Some basic parsing rules that bypass the rest of our logic.
+    if line == '' or line == '***': # Restore split newline // Horizontal rule
+        return line + '\n'
+    
+    # Bold the line
+    if line[0] != '#':
+        line = '#' + line
+    
+    # Uppercase the line, all except URLs. Could probably be made more effective?
+    ldata = re.split(r"(\[.*?\]\(.*?\))", line) # this finds reddit markdown URLs, i.e. [google](http://google.com)
+    line = ''
+    for content in ldata:
+        if not content.startswith('['): # typical string
+            content = content.upper()
+        else:
+            # url; so let's break it up a little further and capitalize the title also
+            url_groups = re.search(r"\[(.*)\]\((.*)\)", content)
+            content = '[' + url_groups.group(1).upper() + '](' + url_groups.group(2) + ')'
+        
+        line += content
+    
+    # Finally, return!
+    return line + '\n'
 
 def isNotAValidComment(thing): #Is it not a valid comment
     return hasattr(thing,"domain") #If it has domain, It's a post, so ignore.
