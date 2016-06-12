18c0d841d5eb8ecd99da5ad149e47be13e8c4bb4
sagiksp <sagi2133.ksp@gmail.com>
2016-06-12 10:27:06 +0300

    Update + comments
    This is the final commit

--- Main.py
+++ Main.py
@@ -1,45 +1,48 @@
 #Made by /u/sagiksp
 
+username="" #BOTS NAME
+password="" #BOTS PASSWORD
+creator_name="" #YOUR NAME (WITHOUT THE /U/)
+bot_subreddit="" #BOTS SUBREDDIT
+
 import praw, time, re
-
-#force single instance - to prevent fuckups
-
-from tendo import singleton
-SingleInstance = singleton.SingleInstance()
 
 #Variables
 
-user_agent=""
-username=""
-password=""
-
-users = [] #Array of all people that used the bot, and times of use. Format: [[Username,UTC_Time],[Username,UTC_Time],...]
-
-yelled = []
-
+user_agent="Cant Hear You by /u/"+creator_name #The name the bot will use to communicate with reddit.
+users = [] #People that have been yelled at. Format: [[Username,UTC_Time],[Username,UTC_Time],...]
+banned_users = ["TheWutBot", "TheWallGrows","AutoModerator"] #Other bots
 Triggers = ('what', 'wut', 'wat', 'wot')
+no_link_subs = ("ImGoingToHellForThis") #Subs that have asked me not to link to the subreddit
 
-footer = """***
+footer = { #text at the end of the post. !normal is the default, !no_link is without links and the rest are for custom subs
+"!normal":"***\n\n##[^^^(I&#32;am&#32;a&#32;bot,&#32;and&#32;I&#32;don't&#32;respond&#32;to&#32;myself.)](https://np.reddit.com/r/"+bot_subreddit+"/)",
 
-[^^^Beep ^^^boop.](https://np.reddit.com/r/CantHearYouBot/)""" #Text to be in the end of a message
+"!no_link":"***\n\n^^^(I&#32;am&#32;a&#32;bot,&#32;and&#32;I&#32;don't&#32;respond&#32;to&#32;myself.)",
 
-def check_condition(c): #Check the bot condition
-    return (c.body.lower().rstrip('?') in Triggers) and (not RateLimit(c.author.name)) #Is the comment a trigger, and is the author not rate limited.
+"totallynotrobots":"***\n\n##[^^^HELLO ^^^FELLOW ^^^HUMANS! ^^^I ^^^AM ^^^A ^^^~~bot~~HUMAN ^^^TOO, ^^^AND ^^^I ^^^DON'T ^^^RESPOND ^^^TO ^^^MYSELF.](https://np.reddit.com/r/"+bot_subreddit+"/)"
+}
+
+def check_condition(c): #The condition for the bot. If this is true, the bot will comment.
+    return (c.body.lower().rstrip('?') in Triggers) and (not RateLimit(str(c.author))) #Is the comment a trigger, and is the author not rate limited.
 
 def bot_action(c,r): #Action the bot preforms
     global users
     parent = r.get_info(thing_id=c.parent_id) #get parent comment
-
+    
+    if str(parent.author) in banned_users or str(c.author) in banned_users: return #If users banned: return
+    
     if isNotAValidComment(parent): return #If it is a response to a thread, stop.
+    subreddit = str(c.subreddit)
 
-    if parent.author.name == username and parent.body != "In da but": #If parent is bot and comment is not "in da but"
+    if str(parent.author) == username and parent.body != "In da but": #If parent is bot and comment is not "in da but"
         return
 
-    users.append([c.author.name,c.created_utc]) #Add username and time of use to the users list
+    users.append([str(c.author),c.created_utc]) #Add username and time of use to the users list
 
 
-    if check_condition(parent): #What What
-        try: #Crashed without this
+    if check_condition(parent): #If both comments are triggers, ie. What What
+        try:
             c.reply("In da but")
         except:
             pass
@@ -48,10 +51,10 @@ def bot_action(c,r): #Action the bot preforms
     lines = parent.body.split("\n") #Split parent into lines
     total = ""
     for line in lines: #for each line
-        total += parseLine(line) #Parse line and add to total
-    try: #Crashes without this
-        c.reply(total + footer) #reply
-        print("\n"+total+"\n\n") #Debug
+        pLine = parseLine(line) #Parse line
+        total += pLine
+    try:
+        c.reply(total + get_footer(subreddit))
     except:
         return
 
@@ -66,19 +69,29 @@ def getLastTimeOfUse(name): #Get last time a username has used the bot
     return findUsersWithName(name)[-1][1]
 
 def RateLimit(username): #Boolean. if user has used the bot in the last 5 minutes, stop.
-    if username == "sagiksp": return False #unlimited testing
+    if username == creator_name: return False #unlimited testing
     c_time = time.time() #Current time
     if findUsersWithName(username) == []: #first time using the bot
         return False #No Rate limiting
     lastUseTime = getLastTimeOfUse(username) #Latest time of use
     return (c_time - lastUseTime) <= 300 #has the user used the bot in the last 300 seconds (5 minutes)
 
-def parseLine(line):
+def get_footer(subreddit): #gets footer via subreddit name
+    ft = ""
+    if subreddit in no_link_subs:
+        ft = footer["!no_link"]
+    elif subreddit in footer:
+        ft = footer[subreddit]
+    else:
+        ft = footer["!normal"]
+    return ft
+
+def parseLine(line): #Written by the awesome sctigercat1
     # Some basic parsing rules that bypass the rest of our logic.
     if line == '' or line == '***': # Restore split newline // Horizontal rule
         return line + '\n'
-    
-    # Bold the line
+     
+     #Bold the line
     if line[0] == ">":
        line = ">#" + line[1:]
     elif line[0] != '#':
@@ -103,9 +116,11 @@ def parseLine(line):
 def isNotAValidComment(thing): #Is it not a valid comment
     return hasattr(thing,"domain") #If it has domain, It's a post, so ignore.
 
+#Bot code
+
 r = praw.Reddit(user_agent) #user_agent
 
-r.login(username=username,password=password)
+r.login(username=username,password=password,disable_warning=True)
 
 for c in praw.helpers.comment_stream(r, 'all'): #for all comments
     if check_condition(c): #If condition
